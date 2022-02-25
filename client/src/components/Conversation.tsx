aaaa6be55c5faf57e5d4bca903cc19fe12a70642
Keshav Kishor Ram <ramabishek40@gmail.com>
2022-02-25 21:11:46 +0545

    Improve bug in responsiveness

--- client/src/components/Conversation.tsx
+++ client/src/components/Conversation.tsx
@@ -70,7 +70,7 @@ const Conversation = () => {
     });
   const conversationViewRef = useRef<any>(null);
   return (
-    <main className="sm:max-h-[500px] h-[500px] max-h-[95%]  bg-slate-700 overflow-y-scroll overflow-x-hidden p-3 pb-8 flex flex-col flex-1 scrollbar-hide rounded-lg transition-all">
+    <main className="bg-slate-700 overflow-y-scroll overflow-x-hidden p-3 pb-8 flex flex-1 flex-col scrollbar-hide rounded-lg transition-all">
       {messages.map(
         (message: {
           text: string;
