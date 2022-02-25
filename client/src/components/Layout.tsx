a72dc088b7f4aeed4369998e9b447f59a61f9bbd
Keshav Kishor Ram <ramabishek40@gmail.com>
2022-02-25 11:26:28 +0545

    Add soft border to layout component

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -4,7 +4,7 @@ const Conversation = React.lazy(() => import("./Conversation"));
 const CreateMessage = React.lazy(() => import("./CreateMessage"));
 const Layout = () => {
   return (
-    <div className="max-w-[600px] min-w-[600px] bg-slate-800 p-3 rounded text-white">
+    <div className="max-w-[600px] min-w-[600px] bg-slate-800 p-3 rounded text-white border border-blue-800/50">
       <div className="chat h-full flex flex-col">
         <Suspense fallback={<h2>Loading...</h2>}>
           <Header />
